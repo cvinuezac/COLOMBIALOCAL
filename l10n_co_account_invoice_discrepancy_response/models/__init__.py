7b213649f305116e22366a0e26f8701f9a75058b
JoanMarin <joanmarinbustamante@gmail.com>
2024-06-20 21:30:40 +0000

    [DEL] l10n_co_account_invoice_discrepancy_response module

--- l10n_co_account_invoice_discrepancy_response/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2024 Joan Marín <Github@JoanMarin>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import account_journal
-from . import account_invoice_discrepancy_response_code
-from . import account_invoice_line
-from . import account_invoice
-from . import account_move
