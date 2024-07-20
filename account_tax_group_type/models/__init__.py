9cf34020d357a448fb269c9d69288ca203ee6505
JoanMarin <joanmarinbustamante@gmail.com>
2024-07-20 13:42:59 +0000

    [DEL] account_tax_group_type module

--- account_tax_group_type/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2024 Joan Marín <Github@JoanMarin>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0).
-
-from . import account_tax_group_type
-from . import account_tax_group
